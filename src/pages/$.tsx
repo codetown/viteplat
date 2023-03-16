ddcd8f1ed7368002d3b1cbb256344c03203cc4a2
Leo Liu <codetown@163.com>
2023-03-16 22:40:56 +0800

    fix bugs

--- src/pages/$.tsx
+++ src/pages/$.tsx
@@ -1,18 +1,21 @@
-import { history } from 'ice';
+import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
 import React from 'react';
 
-const NotFoundPage: React.FC = () => (
+const NotFoundPage: React.FC = () =>{
+  const navigate=useNavigate()
+  return (
   <Result
     status="404"
     title="404"
     subTitle="你访问的页面不存在"
     extra={
-      <Button type="primary" onClick={() => history?.push('/dashboard')}>
+      <Button type="primary" onClick={() => {navigate(-1)}}>
         返回
       </Button>
     }
   />
-);
+)
+};
 
 export default NotFoundPage;
