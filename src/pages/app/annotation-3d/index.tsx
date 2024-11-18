28e3bb59306d6889041e7e31313f2efc4cb6db5d
leo <codetown@163.com>
2024-11-18 22:36:48 +0800

    fix bugs

--- src/pages/app/annotation-3d/index.tsx
+++ src/pages/app/annotation-3d/index.tsx
@@ -41,7 +41,9 @@ export default function () {
                 <ToolBar actions={topActions}/>
                 <ImageList dataSource={imageData}/>
                 <div className={classes.canvasBox}>
+                    <div className={classes.canvasBody}>
                     读取PCD
+                    </div>
                 </div>
             </div>
         </div>
